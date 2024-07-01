ee2e45f50532317ab90eadc47456345631339a75
rishabh jain <rishabh19021998@gmail.com>
2024-07-01 16:45:11 +0530

    project 1

---
+++ 02-advance/01-dom/.js
@@ -0,0 +1,6 @@
+//DOM MANIPULATION
+//document.querySelector('h1') => gives first h1 element
+//document.querySelector('.class)
+//document.querySelector('#id)
+//document.querySelector('')
+//
---
+++ Project/projectset1.md
@@ -0,0 +1,9 @@
+# project related to DOM
+# project link
+[Click here]()
+
+# solution code
+## project 1 
+```javascript
+console.log('hitesh')
+```
